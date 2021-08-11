3f05a60f5740403232f894e5760e2e9c5ed3f13e
zhaoyuanbang <zhaoyuanbang@longfor.com>
2021-08-11 19:07:36 +0800

    ceshi

---
+++ src/components/PureInboxScreen.stories.js
@@ -0,0 +1,42 @@
+import PureInboxScreen from './PureInboxScreen.vue';
+import { app } from '@storybook/vue3';
+import { createStore } from 'vuex';
+
+import { action } from '@storybook/addon-actions';
+import * as TaskListStories from './PureTaskList.stories';
+
+const store = createStore({
+  state: {
+    tasks: TaskListStories.Default.args.tasks,
+  },
+  actions: {
+    pinTask(context, id) {
+      action("pin-task")(id);
+    },
+    archiveTask(context, id) {
+      action("archive-task")(id);
+    },
+  },
+});
+
+app.use(store);
+
+export default {
+  component: PureInboxScreen,
+  title: 'PureInboxScreen',
+};
+
+const Template = args => ({
+  components: { PureInboxScreen },
+  setup() {
+    return {
+      args,
+    };
+  },
+  template: '<PureInboxScreen v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = { error: true };
---
+++ tests/unit/TaskList.spec.js
@@ -0,0 +1,15 @@
+import { mount } from '@vue/test-utils';
+
+import PureTaskList from '../../src/components/PureTaskList.vue'
+
+//👇 Our story imported here
+import { WithPinnedTasks } from '../../src/components/PureTaskList.stories';
+
+test('renders pinned tasks at the start of the list', () => {
+  const wrapper = mount(PureTaskList, {
+    //👇 Story's args used with our test
+    propsData: WithPinnedTasks.args,
+  });
+  const firstPinnedTask = wrapper.find('.list-item:nth-child(1).TASK_PINNED');
+  expect(firstPinnedTask).not.toBe(null);
+});
